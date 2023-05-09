e7911a2c157acec3939519901f0a3667b8dbc4c5
Ravil <Rass00032@yandex.ru>
2023-05-09 21:16:10 +0400

    style: удалены неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
@@ -27,7 +26,6 @@ public class FilmController {
     @PostMapping
     public Film addFilm(@RequestBody @Valid Film film) {
         return storage.addFilm(film);
-
     }
 
     @PutMapping
@@ -36,16 +34,14 @@ public class FilmController {
     }
 
     @GetMapping(value = "/{id}")
-    private Film getFilm(@PathVariable(value = "id") int id){
+    private Film getFilm(@PathVariable(value = "id") int id) {
         return storage.getFilm(id);
     }
-
 
     @GetMapping
     public List<Film> getAllFilms() {
         return storage.getAllFilms();
     }
-
 
     @PutMapping(value = "/{id}/like/{userId}")
     public int addLike(@PathVariable(value = "id") int id, @PathVariable(value = "userId") int userId) {
@@ -59,7 +55,6 @@ public class FilmController {
 
     @GetMapping(value = "/popular")
     public List<Film> popularFilm(@RequestParam(defaultValue = "10", required = false) String count) {
-
         return service.findPopularFilms(Integer.parseInt(count));
     }
 }
